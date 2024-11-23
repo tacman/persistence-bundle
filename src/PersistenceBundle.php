a09388c3dbe6b7d19996d53d94c605a83ac32db8
Tac Tacelosky <tacman@gmail.com>
2024-11-23 06:04:25 -0500

    refactor to use AbstractBundle

--- src/PersistenceBundle.php
+++ src/PersistenceBundle.php
@@ -2,8 +2,20 @@
 
 namespace Fico7489\PersistenceBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Fico7489\PersistenceBundle\EventListener\DoctrineListener;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class PersistenceBundle extends Bundle
+class PersistenceBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $builder->autowire(DoctrineListener::class)
+            ->setAutowired(true)
+            ->setAutoconfigured(true)
+            ->setPublic(true);
+
+        //        $container->import(__DIR__.'/../../config'');
+    }
 }
